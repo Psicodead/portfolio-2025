060ea48bf4a1e6133080f68ba4c149ac1dc0da54
ramon <juan.ramon93@hotmail.com>
2025-10-06 19:06:15 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Under Construction</h1>
+      <div className="card">
+        <p>
+          Currently rebuilding my portfolio, you can contact me on my <a href="https://www.linkedin.com/in/jcramon93/" target="_blank" rel="noreferrer">LinkedIn</a>
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
